a0f2f9d44c6bc3cfbdd003d3d1d199d66d7bee15
Yosra Yarmani <yarmaniyosra@gmail.com>
2023-11-17 16:06:35 +0100

    first commit

---
+++ dcrm/mydb.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+# create db connection
+db = mysql.connector.connect(host="localhost", user="root", password="nx!PJV50514622")
+
+# prepare a cursor object
+cursor = db.cursor()
+
+# create a db
+cursor.execute("CREATE DATABASE elderco")
+
+print("database created")
