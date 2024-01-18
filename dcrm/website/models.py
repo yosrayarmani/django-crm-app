d73976b835e97abbfef62af4d052493461c2d074
Yosra Yarmani <yarmaniyosra@gmail.com>
2024-01-18 23:02:27 +0100

    dataBaseModel

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+  created_at = models.DateTimeField(auto_now_add=True)
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  email = models.CharField(max_length=70)
+  phone = models.CharField(max_length=15)
+  address = models.CharField(max_length=100)
+  city = models.CharField(max_length=50)
+  state = models.CharField(max_length=50)
+  zipcode = models.CharField(max_length=20)
+
+
