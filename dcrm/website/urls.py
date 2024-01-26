16eb7be9eced8329c7468680dc9654d9a0f3419a
Yosra Yarmani <yarmaniyosra@gmail.com>
2024-01-26 15:49:08 +0100

    updateRecord

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
 path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
 
 path('add_record/', views.add_record, name='add_record'),
+
+path('update_record/<int:pk>', views.update_record, name='update_record'),
 ]
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -88,3 +88,18 @@ def add_record(request):
   else:
     messages.success(request, "You must be logged in first to add the record!")
     return redirect('home')
+  
+def update_record(request, pk):
+  if request.user.is_authenticated:
+    current_record = Record.objects.get(id=pk)
+    form = AddRecordForm(request.POST or None, instance=current_record)
+    if request.method=='POST':
+      if form.is_valid():
+        form.save()
+        messages.success(request,"The record was updated successfully!")
+        return redirect("home")
+    return render(request,'update_record.html',{"form":form})
+  else:
+    messages.success(request, "You must be logged in first to update the record!")
+    return redirect('home')
+
